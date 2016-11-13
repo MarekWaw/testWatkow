a763bed39b6c59558d143319a85745e2526991d9
MarekWaw <mwawrzynczyk@windowslive.com>
2016-11-13 12:30:10 +0100

    Popraweczka

--- src/SimpleThreads.java
+++ src/SimpleThreads.java
@@ -2,6 +2,7 @@ public class SimpleThreads {
 
     // Display a message, preceded by
     // the name of the current thread
+	// Wrzucono do GitHuba
     static void threadMessage(String message) {
         String threadName = Thread.currentThread().getName();
         System.out.format("%s: %s%n", threadName, message);
