64065460b1c6dee667c6c6a2884d4d2896048ea4
MarekWaw <mwawrzynczyk@windowslive.com>
2016-11-13 18:04:40 +0100

    Popoludniu

--- src/SimpleThreads.java
+++ src/SimpleThreads.java
@@ -4,6 +4,7 @@ public class SimpleThreads {
     // the name of the current thread
 	// Wrzucono do GitHuba
 	// Druga poprawka, trzecia poprawka
+	// Nowa
     static void threadMessage(String message) {
         String threadName = Thread.currentThread().getName();
         System.out.format("%s: %s%n", threadName, message);
