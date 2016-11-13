553e1440990f6fd8f7a0c0cc50f02bb6e7a3db91
Marek Wawrzynczyk <mwawrzynczyk@windowslive.com>
2016-11-13 18:06:03 +0100

    Update SimpleThreads.java
    Na serwerze

--- src/SimpleThreads.java
+++ src/SimpleThreads.java
@@ -5,6 +5,7 @@ public class SimpleThreads {
 	// Wrzucono do GitHuba
 	// Druga poprawka, trzecia poprawka
 	// Nowa
+	// Poprawka na serwerze
     static void threadMessage(String message) {
         String threadName = Thread.currentThread().getName();
         System.out.format("%s: %s%n", threadName, message);
